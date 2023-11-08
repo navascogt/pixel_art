90cb1372501b2af131038019ee7a66afc59fcb7e
Douglas Navas <douglas.l@navascogt.com>
2023-11-08 02:18:48 +0000

    Create pixel_art package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='pixel_art',
+   version='0.1',
+   description='Package for drawing pixel art to the terminal.',
+   author='Douglas Navas',
+   packages=['pixel_art'],
+   install_requires = ['colorama']
+)
